fd08613880ff5a2f10eb179332ba7126529a47c9
hayzamjs <hayzam@gmail.com>
2024-09-07 23:42:28 +0000

    turn: more fixes for ipv6

--- go.mod
+++ go.mod
@@ -3,16 +3,18 @@ module github.com/AlchemillaHQ/simple-turn
 go 1.22.1
 
 require (
-	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+	github.com/pion/turn/v4 v4.0.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
 	github.com/pion/dtls/v3 v3.0.2 // indirect
 	github.com/pion/logging v0.2.2 // indirect
 	github.com/pion/randutil v0.1.0 // indirect
 	github.com/pion/stun/v3 v3.0.0 // indirect
 	github.com/pion/transport/v3 v3.0.7 // indirect
-	github.com/pion/turn/v4 v4.0.0 // indirect
-	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/wlynxg/anet v0.0.4 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
-	gortc.io/stun v1.23.0 // indirect
 )
--- turn.go
+++ turn.go
@@ -4,96 +4,142 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strings"
+	"sync"
 	"time"
-
 	"github.com/pion/turn/v4"
-	"github.com/sirupsen/logrus"
 )
 
 var httpClient = &http.Client{Timeout: 5 * time.Second}
 
+type customRelayAddressGenerator struct {
+	relayAddress net.IP
+	address      string
+	isIPv6       bool
+}
+
+func (g *customRelayAddressGenerator) AllocatePacketConn(network string, requestedPort int) (net.PacketConn, net.Addr, error) {
+	var conn net.PacketConn
+	var err error
+	var listenAddress string
+
+	if g.isIPv6 {
+		listenAddress = fmt.Sprintf("[%s]:%d", g.address, requestedPort)
+		conn, err = net.ListenPacket("udp6", listenAddress)
+	} else {
+		listenAddress = fmt.Sprintf("%s:%d", g.address, requestedPort)
+		conn, err = net.ListenPacket("udp4", listenAddress)
+	}
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return conn, conn.LocalAddr(), nil
+}
+
+func (g *customRelayAddressGenerator) AllocateConn(network string, requestedPort int) (net.Conn, net.Addr, error) {
+	return nil, nil, fmt.Errorf("AllocateConn not implemented")
+}
+
+func (g *customRelayAddressGenerator) Validate() error {
+	if g.relayAddress == nil {
+		return fmt.Errorf("relay address is nil")
+	}
+	if g.address == "" {
+		return fmt.Errorf("address is empty")
+	}
+	return nil
+}
+
 func StartServer(config *Config) error {
-	// Parse IPv4 address
-	ipv4, portv4, err := net.SplitHostPort(config.IPv4Bind)
+	var wg sync.WaitGroup
+	errChan := make(chan error, 2)
+
+	// Start IPv4 server
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := startTURNServer(config, false); err != nil {
+			errChan <- fmt.Errorf("IPv4 server error: %v", err)
+		}
+	}()
+
+	// Start IPv6 server
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := startTURNServer(config, true); err != nil {
+			errChan <- fmt.Errorf("IPv6 server error: %v", err)
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+
+	for err := range errChan {
+		return err
+	}
+
+	select {}
+}
+
+func startTURNServer(config *Config, isIPv6 bool) error {
+	var bindAddress, publicIP string
+	var udpListener net.PacketConn
+	var err error
+
+	if isIPv6 {
+		bindAddress = config.IPv6Bind
+		publicIP, _, err = net.SplitHostPort(bindAddress)
+		publicIP = strings.Trim(publicIP, "[]")
+		udpListener, err = net.ListenPacket("udp6", bindAddress)
+	} else {
+		bindAddress = config.IPv4Bind
+		publicIP, _, err = net.SplitHostPort(bindAddress)
+		udpListener, err = net.ListenPacket("udp4", bindAddress)
+	}
+
 	if err != nil {
-		return fmt.Errorf("failed to parse IPv4 bind address: %v", err)
-	}
-	ipv4Addr := net.ParseIP(ipv4)
-	if ipv4Addr == nil {
-		return fmt.Errorf("invalid IPv4 address: %s", ipv4)
+		return fmt.Errorf("failed to create UDP listener: %v", err)
 	}
 
-	// Parse IPv6 address
-	ipv6, portv6, err := net.SplitHostPort(config.IPv6Bind)
-	if err != nil {
-		return fmt.Errorf("failed to parse IPv6 bind address: %v", err)
-	}
-	ipv6Addr := net.ParseIP(ipv6)
-	if ipv6Addr == nil {
-		return fmt.Errorf("invalid IPv6 address: %s", ipv6)
+	relayIP := net.ParseIP(publicIP)
+	if relayIP == nil {
+		return fmt.Errorf("failed to parse IP address: %s", publicIP)
 	}
 
-	realm := config.Realm
-
-	serverConfig := turn.ServerConfig{
-		Realm: realm,
+	s, err := turn.NewServer(turn.ServerConfig{
+		Realm: config.Realm,
 		AuthHandler: func(username string, realm string, srcAddr net.Addr) ([]byte, bool) {
 			resp, err := httpClient.Get(fmt.Sprintf("%s/%s", config.AuthEndpoint, username))
 			if err != nil {
-				logrus.Errorf("Failed to contact auth server: %v", err)
 				return nil, false
 			}
 			defer resp.Body.Close()
 			if resp.StatusCode != http.StatusOK {
-				logrus.Infof("Authentication failed for token %s", username)
 				return nil, false
-			} else {
-				logrus.Debugf("Authentication succeeded for token %s", username)
 			}
 			return turn.GenerateAuthKey(username, realm, username), true
 		},
-	}
-
-	// IPv4 configuration
-	if ipv4Addr != nil {
-		udpListenerIPv4, err := net.ListenPacket("udp4", net.JoinHostPort(ipv4, portv4))
-		if err != nil {
-			return fmt.Errorf("failed to create UDP listener for IPv4: %v", err)
-		}
-		defer udpListenerIPv4.Close()
-
-		serverConfig.PacketConnConfigs = append(serverConfig.PacketConnConfigs, turn.PacketConnConfig{
-			PacketConn: udpListenerIPv4,
-			RelayAddressGenerator: &turn.RelayAddressGeneratorStatic{
-				RelayAddress: ipv4Addr,
-				Address:      "0.0.0.0",
+		PacketConnConfigs: []turn.PacketConnConfig{
+			{
+				PacketConn: udpListener,
+				RelayAddressGenerator: &customRelayAddressGenerator{
+					relayAddress: relayIP,
+					address:      publicIP,
+					isIPv6:       isIPv6,
+				},
 			},
-		})
-		logrus.Infof("STUN/TURN server is running on %s (IPv4)", config.IPv4Bind)
-	}
+		},
+	})
 
-	// IPv6 configuration
-	if ipv6Addr != nil {
-		udpListenerIPv6, err := net.ListenPacket("udp6", net.JoinHostPort(ipv6, portv6))
-		if err != nil {
-			return fmt.Errorf("failed to create UDP listener for IPv6: %v", err)
-		}
-		defer udpListenerIPv6.Close()
-
-		serverConfig.PacketConnConfigs = append(serverConfig.PacketConnConfigs, turn.PacketConnConfig{
-			PacketConn: udpListenerIPv6,
-			RelayAddressGenerator: &turn.RelayAddressGeneratorStatic{
-				RelayAddress: ipv6Addr,
-				Address:      "::",
-			},
-		})
-		logrus.Infof("STUN/TURN server is running on %s (IPv6)", config.IPv6Bind)
-	}
-
-	_, err = turn.NewServer(serverConfig)
 	if err != nil {
-		return fmt.Errorf("failed to create STUN/TURN server: %v", err)
+		return fmt.Errorf("failed to create TURN server: %v", err)
 	}
 
+	defer s.Close()
 	select {}
 }
